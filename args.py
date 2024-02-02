ec2c2d3d282c2ac69a1c91fa9543239c5296a3ac
eirinko <eirinkorvald.ek@gmail.com>
2024-02-02 13:55:31 +0100

    created args.py

---
+++ args.py
@@ -0,0 +1,34 @@
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='simple args')
+
+parser.add_argument('-s' , '--server', action='store_true')
+parser.add_argument('-c' , '--client', action='store_true')
+parser.add_argument('-p', '--port', type=int, default=8088)
+parser.add_argument('-i', '--ip', type=str, default="10.0.0.2")
+
+args = parser.parse_args()
+
+#Setting up to test if the IP address is in the correct format
+test_ip = args.ip.split(".")
+notinrange = False
+for number in test_ip:
+    if int(number) not in range (0,255):
+        notinrange = True
+
+#Check for the range of the port
+if args.port not in range(1024,65535):
+    print("Invalid port. It must be within the range [1024,65535]")
+#Check for the format of the IP address
+elif len(test_ip)!=4 or notinrange:
+    print("Invalid IP. It must in this format: 10.1.2.3")
+else:
+    if args.server and args.client:
+        print("You cannot use both at the same time")
+    elif args.server:
+        print(f"The server is running with IP address = {args.ip} and port address = {args.port}")
+    elif args.client:
+        print(f"The client is running with IP address = {args.ip} and port address = {args.port}")
+    else:
+        print("You should run either in server or client mode")
