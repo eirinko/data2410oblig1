39ea893810528e9c5722523737da772547b33b58
Eirin Korvald <31256905+eirinko@users.noreply.github.com>
2024-02-07 09:54:34 +0100

    Update jains.py

--- src/jains.py
+++ src/jains.py
@@ -1,12 +1,16 @@
 import sys
 
+'''
+    Description jfi(throughputlist) takes a list of throughput values for connections
+    and calculates the Jains Fairness Index (JFI). 
+'''
 def jfi(throughputlist):
     #Some unit tests
     """
     >>> jfi([10,10,10])
     1.0
     >>> jfi(['bla',10,10])
-    bla is not a value and will not be added
+    bla is not an integer and will not be added
     1.0
     >>> jfi([5,10])
     0.9
@@ -31,19 +35,24 @@ def jfi(throughputlist):
             denominator += number*number
             n+=1
         except:
-            print(f"{throughputlist[i]} is not a value and will not be added")
+            #In case the list contains elements that can't be converted into integer.
+            print(f"{throughputlist[i]} is not an integer and will not be added")
     if (n == 0 or denominator == 0):
+        #In case the list contains nothing.
         raise ZeroDivisionError
     return (numerator**2)/(n*denominator)
 
+#Try to use the function jfi(throughputlist) with the given input
 try:
-    text = sys.argv[1] #Taking the input that comes after the name of the file.
+    #Taking the input that comes after the name of the file that gets run.
+    text = sys.argv[1] 
     with open(text) as f:
         #Removes new line characters and adds info to a list.
         lines = [line.rstrip() for line in f]
     result = jfi(lines)
     print(f'Jains Fairness index is: {result}')
 except:
+    #Error message in case the user forgets to provide input. 
     print("Either you forgot to provide a file as input in the terminal or your file is empty.")
 
 if __name__ == "__main__":
